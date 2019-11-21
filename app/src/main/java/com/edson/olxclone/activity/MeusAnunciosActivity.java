5b50b7ec4a969486e79515d19692e87ece6b9aaa
EdsonGomesJR <edyz86@hotmail.com>
2019-11-21 11:00:36 -0300

    Implementação do Adapter para anuncios configuracao do recyclerView exibir anuncios do usuario no feed de meus anuncios

--- app/src/main/java/com/edson/olxclone/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/edson/olxclone/activity/MeusAnunciosActivity.java
@@ -3,22 +3,48 @@ package com.edson.olxclone.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.edson.olxclone.adapter.AdapterAnuncios;
+import com.edson.olxclone.helper.ConfiguracaoFirebase;
+import com.edson.olxclone.model.Anuncio;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import com.edson.olxclone.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private DatabaseReference anuncioUsuarioRef;
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncioList = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
+
+        //config iniciais
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child(ConfiguracaoFirebase.getIdUsuario());
+
+        inicializarComponentes();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,11 +53,48 @@ public class MeusAnunciosActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(getApplicationContext(),CadastrarAnuncioActivity.class                                                                                                                                 ));
+                startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class));
 
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //config recyclerView
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncioList, this);
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        //recuperar anuncios para o user
+        recuperarAnuncios();
+    }
+
+    private void recuperarAnuncios() {
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                anuncioList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    anuncioList.add(ds.getValue(Anuncio.class));
+
+                }
+                Collections.reverse(anuncioList);
+                adapterAnuncios.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void inicializarComponentes() {
+
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 
 }
