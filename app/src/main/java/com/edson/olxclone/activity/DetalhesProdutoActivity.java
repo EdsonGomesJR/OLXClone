e58144dcdec1f008451e1db0e63a9ea5693ca551
EdsonGomesJR <edyz86@hotmail.com>
2019-11-26 10:47:44 -0300

    configuração da toolbar

--- app/src/main/java/com/edson/olxclone/activity/DetalhesProdutoActivity.java
+++ app/src/main/java/com/edson/olxclone/activity/DetalhesProdutoActivity.java
@@ -23,6 +23,9 @@ public class DetalhesProdutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produto);
 
+        //config toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         inicializarComponentes();
 
         //recuperar anuncio para exibição
