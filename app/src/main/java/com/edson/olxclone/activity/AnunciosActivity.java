2f5eee95c6074b58bef3327a8ed1882e45aeb261
EdsonGomesJR <edyz86@hotmail.com>
2019-11-26 10:04:30 -0300

    config do metodo para recuperar os anuncios filtrados no spinner de categorias

--- app/src/main/java/com/edson/olxclone/activity/AnunciosActivity.java
+++ app/src/main/java/com/edson/olxclone/activity/AnunciosActivity.java
@@ -10,12 +10,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.edson.olxclone.R;
 import com.edson.olxclone.adapter.AdapterAnuncios;
@@ -25,6 +27,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -35,7 +38,7 @@ import dmax.dialog.SpotsDialog;
 
 public class AnunciosActivity extends AppCompatActivity {
 
-    private FirebaseAuth autenticacao ;
+    private FirebaseAuth autenticacao;
     private DatabaseReference anuncioPublicoRef;
     private Button buttonRegiao, buttonCategoria;
     private RecyclerView recyclerViewAnunciosPublicos;
@@ -43,7 +46,8 @@ public class AnunciosActivity extends AppCompatActivity {
     private List<Anuncio> anuncioList = new ArrayList<>();
     private AlertDialog dialog;
     private String filtroEstado = "";
-
+    private String filtroCategoria = "";
+    private boolean filtrandoPorEstado = false;
 
 
     @Override
@@ -93,6 +97,7 @@ public class AnunciosActivity extends AppCompatActivity {
 
                 filtroEstado = spinnerEstado.getSelectedItem().toString();
                 recuperarAnunciosPorEstado();
+                filtrandoPorEstado = true;
 
             }
         });
@@ -106,6 +111,114 @@ public class AnunciosActivity extends AppCompatActivity {
 
         AlertDialog dialog = dialogEstado.create();
         dialog.show();
+
+    }
+
+    public void filtrarPorCategoria(View view) {
+
+        if (filtrandoPorEstado == true) {
+            AlertDialog.Builder dialogEstado = new AlertDialog.Builder(this);
+            dialogEstado.setTitle("Selecione a Categoria desejada");
+
+            //configurar spinner
+            View viewSpinner = getLayoutInflater().inflate(R.layout.dialog_spinner, null);
+
+            //spinner de estados
+            final Spinner spinnerCategoria = viewSpinner.findViewById(R.id.spinnerFiltro);
+            String[] categorias = getResources().getStringArray(R.array.categoria);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_spinner_item, categorias);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerCategoria.setAdapter(adapter);
+
+            dialogEstado.setView(viewSpinner);
+            dialogEstado.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    filtroCategoria = spinnerCategoria.getSelectedItem().toString();
+                    recuperarAnuncioPorCategoria();
+
+                }
+            });
+
+            dialogEstado.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            AlertDialog dialog = dialogEstado.create();
+            dialog.show();
+
+        } else {
+
+            Toast.makeText(this, "Escolha primeiro uma região", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void recuperarAnuncioPorCategoria() {
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setTheme(R.style.Custom)
+                .setMessage("Carregando Anúncios..")
+                .setCancelable(false)
+                .build();
+        dialog.show();
+
+        //config nó por categoria
+        anuncioPublicoRef = ConfiguracaoFirebase.getFirebase()
+                .child("anuncios")
+                .child(filtroEstado)
+                .child(filtroCategoria);
+
+        anuncioPublicoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                anuncioList.clear();
+                /** String categoriaNome;
+                 anuncioList.clear();
+
+                 for (DataSnapshot estados : dataSnapshot.getChildren()) {
+                 for (DataSnapshot categorias : estados.getChildren()) {
+                 categoriaNome = categorias.getKey();
+
+                 if (filtroCategoria.equals(categoriaNome)) {
+
+                 for (DataSnapshot anuncios : categorias.getChildren()) {
+
+                 Anuncio anuncio = anuncios.getValue(Anuncio.class);
+                 anuncioList.add(anuncio);
+                 }
+                 }
+                 }
+                 }
+
+                 Collections.reverse(anuncioList);
+                 adapterAnuncios.notifyDataSetChanged();*/ //modo somente para retornar as categorias
+
+                for (DataSnapshot anuncios : dataSnapshot.getChildren()) {
+
+
+                    Anuncio anuncio = anuncios.getValue(Anuncio.class);
+                    anuncioList.add(anuncio);
+                }
+
+                Collections.reverse(anuncioList);
+                adapterAnuncios.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 
@@ -164,12 +277,11 @@ public class AnunciosActivity extends AppCompatActivity {
                 .build();
         dialog.show();
 
-        anuncioList.clear();
 
         anuncioPublicoRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                anuncioList.clear();
                 for (DataSnapshot estados : dataSnapshot.getChildren()) {
                     for (DataSnapshot categorias : estados.getChildren()) {
                         for (DataSnapshot anuncios : categorias.getChildren()) {
@@ -206,14 +318,14 @@ public class AnunciosActivity extends AppCompatActivity {
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
 
-        if(autenticacao.getCurrentUser() == null){
+        if (autenticacao.getCurrentUser() == null) {
             //user deslogado
-            menu.setGroupVisible(R.id.group_deslogado,true);
+            menu.setGroupVisible(R.id.group_deslogado, true);
 
-        }else{
-        //user logado
+        } else {
+            //user logado
 
-            menu.setGroupVisible(R.id.group_logado,true);
+            menu.setGroupVisible(R.id.group_logado, true);
 
         }
         return super.onPrepareOptionsMenu(menu);
@@ -223,7 +335,7 @@ public class AnunciosActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_cadastrar:
-                startActivity( new Intent(getApplicationContext(), CadastroActivity.class));
+                startActivity(new Intent(getApplicationContext(), CadastroActivity.class));
 
                 break;
 
@@ -234,7 +346,7 @@ public class AnunciosActivity extends AppCompatActivity {
 
             case R.id.menu_anuncios:
 
-                startActivity(new Intent(getApplicationContext(),MeusAnunciosActivity.class));
+                startActivity(new Intent(getApplicationContext(), MeusAnunciosActivity.class));
                 break;
         }
         return super.onOptionsItemSelected(item);
