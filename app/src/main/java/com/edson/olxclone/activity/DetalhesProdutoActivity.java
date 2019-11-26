3fb6e951a9d9474456314389214cc608b2b448fd
EdsonGomesJR <edyz86@hotmail.com>
2019-11-26 10:44:17 -0300

    configurando a exibição das imagens e ajustes no layout

--- app/src/main/java/com/edson/olxclone/activity/DetalhesProdutoActivity.java
+++ app/src/main/java/com/edson/olxclone/activity/DetalhesProdutoActivity.java
@@ -3,14 +3,59 @@ package com.edson.olxclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.edson.olxclone.R;
+import com.edson.olxclone.model.Anuncio;
+import com.squareup.picasso.Picasso;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class DetalhesProdutoActivity extends AppCompatActivity {
+
+    private CarouselView carouselView;
+    private TextView titulo, descricao, estado, preco;
+    private Anuncio anuncioSelecionado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produto);
+
+        inicializarComponentes();
+
+        //recuperar anuncio para exibição
+        anuncioSelecionado = (Anuncio) getIntent().getSerializableExtra("anuncioSelecionado");
+
+        if (anuncioSelecionado != null) {
+
+            titulo.setText(anuncioSelecionado.getTitulo());
+            descricao.setText(anuncioSelecionado.getDescricao());
+            estado.setText(anuncioSelecionado.getEstado());
+            preco.setText(anuncioSelecionado.getValor());
+
+            ImageListener imageListener = new ImageListener() {
+                @Override
+                public void setImageForPosition(int position, ImageView imageView) {
+                    String urlString = anuncioSelecionado.getFotos().get(position);
+
+                    Picasso.get().load(urlString).into(imageView);
+
+                }
+            };
+
+            carouselView.setPageCount(anuncioSelecionado.getFotos().size());
+            carouselView.setImageListener(imageListener);
+        }
+    }
+
+    private void inicializarComponentes() {
+
+        carouselView = findViewById(R.id.carouselView);
+        titulo = findViewById(R.id.textTituloDetalhe);
+        descricao = findViewById(R.id.textDescricaoDetalhe);
+        estado = findViewById(R.id.textEstadoDetalhe);
+        preco = findViewById(R.id.textPrecoDetalhe);
     }
 }
